99a7fd932e2d6bb72ee3a30e883f65d59133ed1f
swaroopRajwal <swaroop.rajwal18@gmail.com>
2022-06-20 19:39:47 +0530

    ✨ 😅 added comments

--- fonts.sh
+++ fonts.sh
@@ -6,17 +6,17 @@ echo $(git clone https://github.com/swaroopRajwal/fonts.git)
 # todo - creating fonts directory and zipping each font in /.local/share/fonts
 files=`ls fonts/*.zip`
 
-# * checking if the fonts directory exists in 
-# * /home/user/.local/share/fonts
-# * if it does then proceeding further
-# * if it doesn't then creating it XD
+# ? checking if the fonts directory exists in 
+# ? /home/user/.local/share/fonts
+# ? if it does then proceeding further
+# ? if it doesn't then creating it XD
 if [[ -d "/home/$USER/.local/share/fonts/" ]]; then
   echo "fonts directory exitst"
   else 
     echo $(cd /home/$USER/.local/share/ && mkdir fonts/)
 fi
 
-
+# * extracting the files in /home/user/.local/share/fonts 
 for entry in $files
 do
   file_name=$(basename "$entry")
@@ -27,5 +27,5 @@ do
   echo $(unzip $entry -d $font_dir)
 done
 
-# deleting the cloned repo
+# * deleting the cloned repo
 echo $(rm -rf fonts)
