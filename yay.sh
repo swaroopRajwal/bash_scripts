6b0347dd0bc2f20295f45ac996adbd64dde91612
swaroopRajwal <swaroop.rajwal18@gmail.com>
2022-06-21 01:42:22 +0530

    😐 forgot a damn space lol

--- yay.sh
+++ yay.sh
@@ -4,5 +4,5 @@
 
 echo $(sudo pacman -S --needed git base-devel)
 echo $(git clone https://aur.archlinux.org/yay-bin.git)
-echo $(cd yay-bin && makepkg-si)
+echo $(cd yay-bin && makepkg -si)
 echo $(rm -rf yay-bin)
